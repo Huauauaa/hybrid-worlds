0fa9b1880fee42f1347b9d089590610c7376c257
Harvey <harvey0379@163.com>
2022-12-03 01:14:25 +0800

    build: base->publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi'
 
 export default defineConfig({
-  base: '/hybrid-worlds/',
+  publicPath: '/hybrid-worlds/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'hybrid-worlds',
